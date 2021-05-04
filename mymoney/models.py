ca31141cfade27898d452cba20c8d41bb1c382b7
zemichaelmd <zemichaelmd@example.com>
2021-05-04 14:11:46 +0300

    Added Exel Data, Made Backups

--- mymoney/models.py
+++ mymoney/models.py
@@ -48,8 +48,8 @@ class Income(models.Model):
     income_date = models.DateTimeField()
     income_amount = models.DecimalField(max_digits=10, decimal_places=2)
     income_description = models.CharField(max_length=350)
-    income_note = models.CharField(max_length=350)
-    income_account = models.ForeignKey(MoneyAccount, on_delete=models.CASCADE)
+    income_note = models.CharField(max_length=350, null=True, blank=True)
+    income_account = models.ForeignKey(MoneyAccount, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return str(self.income_amount) + " from " + self.income_description
--- mymoney/views.py
+++ mymoney/views.py
@@ -4,20 +4,16 @@ from django.http import HttpResponse
 from . import models
 
 # Create your views here.
-
-
 def read(request, pk):
     income = models.Income.objects.get(id=pk)
     context = {'income': income}
     return render(request, 'mymoney/list.html', context)
-
-
 def index(request):
     expences = models.Expense.objects.all()
     context = {'expences': expences}
     return render(request, 'mymoney/list.html', context)
 
-
+'''
 def addFromExel(request):
     import xlrd
     import datetime
@@ -29,19 +25,98 @@ def addFromExel(request):
 
     book = xlrd.open_workbook(file_path)
 
-    # getting the workbook
+    #Adding Expences
     sheet_expence = book.sheet_by_name('Expences')
+
+    # printing how many rows the workbook has
+    # print(sheet.nrows)
+
+    for i in range(sheet_expence.nrows):
+        # row_header_set contains all the data as a set
+        row_header_set = sheet_expence.row_values(i)
+
+        #print each Row
+        #print(row_header_set)
+
+        # print nested cells
+        # for cell in row:
+        # print (cell)
+
+        # This returns the pure data!!
+        try:
+            #Grabbing Date
+            date = int(row_header_set[0])
+            #convert exel Date to Python
+            date_as_datetime = datetime.datetime(*xlrd.xldate_as_tuple(date, book.datemode))
+
+            data = {'Date': date_as_datetime}
+
+            #Datetime Converter to Django Date time
+            import datetime
+            from django.conf import settings
+            from django.utils.timezone import make_aware
+
+            exexpence_date_formated = make_aware(date_as_datetime)
+
+            #Add Expence
+            p = Expense(expense_date = exexpence_date_formated, expense_category=str(row_header_set[1]),
+                         expense_amount=float(row_header_set[2]), expense_description=str(row_header_set[3]),
+                         expense_note=str(row_header_set[4]))
+            p.save()
+            print("done adding" + str(row_header_set[3]))
+
+        except:
+            pass
+
+    #Adding Income
     sheet_income = book.sheet_by_name('Income')
+
+    # printing how many rows the workbook has
+    # print(sheet.nrows)
+
+    for i in range(sheet_income.nrows):
+        # row_header_set contains all the data as a set
+        row_header_set = sheet_income.row_values(i)
+
+        # print each Row
+        #print(row_header_set)
+
+        # print nested cells
+        # for cell in row:
+        # print (cell)
+
+        # This returns the pure data!!
+        try:
+            # Grabbing Date
+            date = int(row_header_set[0])
+            # convert exel Date to Python
+            date_as_datetime = datetime.datetime(*xlrd.xldate_as_tuple(date, book.datemode))
+
+
+            # Datetime Converter to Django Date time
+            from django.utils.timezone import make_aware
+            expence_date_formated = make_aware(date_as_datetime)
+
+            # Add income
+            try:
+                models.Income.objects.create(income_date=expence_date_formated, income_amount=float(row_header_set[2]),
+                              income_description=str(row_header_set[3]))
+            except: print("error!")
+            print("done adding" + str(row_header_set[3]))
+
+        except:
+            pass
+
+
+    #Adding Accounts
     sheet_account = book.sheet_by_name('Accounts')
 
     # printing how many rows the workbook has
     # print(sheet.nrows)
 
-    data = {}
-
-    for i in range(sheet.nrows):
+    for i in range(sheet_account.nrows):
         # row_header_set contains all the data as a set
-        row_header_set = sheet.row_values(i)
+        row_header_set = sheet_account.row_values(i)
 
         #print each Row
         #print(row_header_set)
@@ -72,39 +147,52 @@ def addFromExel(request):
                          expense_note=str(row_header_set[4]))
             p.save()
 
-            #print Data
-            print("Date" + str(date_as_datetime))
-            print("Category : " + str(row_header_set[1]))
-            print("Amount : " + str(row_header_set[2]))
-            print("Reason : " + str(row_header_set[3]))
-            print("Remark : " + str(row_header_set[4]))
-            x = "*** Done Adding ***"
-            print(x)
         except:
             pass
 
+
+        # Adding Transfers
+        sheet_account = book.sheet_by_name('Accounts')
+
+        # printing how many rows the workbook has
+        # print(sheet.nrows)
+
+        for i in range(sheet_account.nrows):
+            # row_header_set contains all the data as a set
+            row_header_set = sheet_account.row_values(i)
+
+            # print each Row
+            # print(row_header_set)
+
+            # print nested cells
+            # for cell in row:
+            # print (cell)
+
+            # This returns the pure data!!
+            try:
+                # Grabbing Date
+                date = int(row_header_set[0])
+                # convert exel Date to Python
+                date_as_datetime = datetime.datetime(*xlrd.xldate_as_tuple(date, book.datemode))
+
+                data = {'Date': date_as_datetime}
+
+                # Datetime Converter to Django Date time
+                import datetime
+                from django.conf import settings
+                from django.utils.timezone import make_aware
+
+                exexpence_date_formated = make_aware(date_as_datetime)
+
+                # Add Expence
+                p = Expense(expense_date=exexpence_date_formated, expense_category=str(row_header_set[1]),
+                            expense_amount=float(row_header_set[2]), expense_description=str(row_header_set[3]),
+                            expense_note=str(row_header_set[4]))
+                p.save()
+
+            except:
+                pass
+
     done = "Done adding..."
     context = {'done': done}
-    print(data)
-    return render(request, 'mymoney/done.html', context)
-    '''JSON
-    {
-        {Date: the date, Category: Lost, Amount: 120.0, Reason: Lost or unknown}
-    }'''
-
-    # count += 1
-
-    '''for sheet in book.sheets():
-        print (sheet.name)'''
-
-    '''
-    read as date
-
-    import datetime, xlrd
-    book = xlrd.open_workbook("myexcelfile.xls")
-    sh = book.sheet_by_index(0)
-    a1 = sh.cell_value(rowx=0, colx=0)
-    a1_as_datetime = datetime.datetime(*xlrd.xldate_as_tuple(a1, book.datemode))
-    print 'datetime: %s' % a1_as_datetime
-
-    '''
+    return render(request, 'mymoney/done.html', context)'''
